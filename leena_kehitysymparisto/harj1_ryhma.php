b13201111967516d6e70d4cdd97f7ded8864c91d
Kasmoi <35563413+Kasmoi@users.noreply.github.com>
2018-12-12 12:49:14 +0200

    Add files via upload

---
+++ leena_kehitysymparisto/harj1_ryhma.php
@@ -0,0 +1,23 @@
+<?php  
+if(isset($_GET["Ryhma"])) $Ryhma=$_GET["Ryhma"];  
+$host ="magnesium";  
+$user = "16bkasmirm";  
+$pass = "salasana";  
+$dbname = "db16bkasmirm";  
+
+try  //yritetään ottaa yhteys  
+{   
+    $yhteys = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass); //luo PDO-olion  
+}   
+catch(PDOException $e) // jos ei onnistu (poikkeus)  
+{   
+    echo $e->getMessage(); //antaa ilmoituksen siitä, missä virhe  
+}  
+
+$sql = "SELECT * FROM tuotteet WHERE Ryhma ='$Ryhma'";  
+$vastaus = $yhteys->prepare($sql);  
+$vastaus->execute();  
+$vastaukset = $vastaus->fetchAll(PDO::FETCH_ASSOC);  
+
+echo json_encode($vastaukset);
+?>  
